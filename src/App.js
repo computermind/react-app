6ff4188826bccd1e29ce228e2f7e3dc39a360316
thomasguwor <guworthomas@gmail.com>
2021-06-05 19:11:07 -0400

    create menucomponent and shared dishes

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Weeglo</NavbarBrand>
-        </div>
-      </Navbar>
+class App extends Component {
 
-      <Menu />
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
 
-    </div>
-  );
+  render(){
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Weeglo</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
